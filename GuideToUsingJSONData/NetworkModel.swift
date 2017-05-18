691e78857cb73399a52f60e7dfad2db2b2b58586
admin <olympiad1980@gmail.com>
2017-05-18 21:08:48 +0300

    use Clousere

--- GuideToUsingJSONData/NetworkModel.swift
+++ GuideToUsingJSONData/NetworkModel.swift
@@ -1,5 +1,7 @@
+import UIKit
+import SwiftyJSON
 
-struct NetworkModel {
+struct Network {
 
     let author: String
     let title: String
@@ -7,17 +9,51 @@ struct NetworkModel {
     
     enum SerializationError: Error {
         
-        case missing(String)
-        case invalid(String, Any)
-    }
-    
-    init( json:[String:Any]) throws {
-        guard let author = json["author"] as? String else {throw SerializationError.missing("summary is missing")}
-        guard let title = json["author"] as? String else {throw SerializationError.missing("title is missing")}
-        guard let urlToImage = json["author"] as? String else {throw SerializationError.missing("utlToImage is missing")}
-        
-        self.author = author
-        self.title = title
-        self.urlToImage = urlToImage
+        case error(Error)
+        case data(Data)
     }
 }
+
+class  NewsArticle {
+    
+    var network = [Network]()
+    
+    static let basePath = "https://newsapi.org/v1/articles?source=al-jazeera-english&sortBy=top&apiKey=4f3ad7637a534b788fcba09428886b62"
+    func forecast(complection: @escaping ([Network], Error?) -> Void) {
+        let url = NewsArticle.basePath
+        let request = URLRequest(url: URL(string: url)!)
+        URLSession.shared.dataTask(with: request) { (data: Data?, _, error: Error?) in
+            
+            guard let data = data else {
+                print("No data!")
+                return
+            }
+            
+            let json = JSON(data: data)
+            if let jsonArticles = json["articles"].array {
+                
+                for pars in jsonArticles {
+                    
+                    let author = pars["author"].stringValue
+                    let title = pars["title"].stringValue
+                    let urlToImage = pars["urlToImage"].stringValue
+                    
+                    let array = Network(author: author, title: title, urlToImage: urlToImage)
+                    self.network.append(array)
+                }
+                
+                print("article: \(self.network)")
+            }
+            
+            DispatchQueue.main.async {
+                complection(self.network, error)
+            }
+            
+        }.resume()
+    }
+}
+
+
+
+
+
--- GuideToUsingJSONData/StartVC.swift
+++ GuideToUsingJSONData/StartVC.swift
@@ -3,11 +3,21 @@ import UIKit
 
 class StartVC: UIViewController {
     
-    let network: [NetworkModel]? = nil
+    let article = NewsArticle()
+    
+    @IBOutlet weak var author: UILabel!
+    @IBOutlet weak var urlImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupArticle()
+    }
+    
+    func setupArticle() {
+        article.forecast { (network, error) in
+            
+        }
     }
 }
 
